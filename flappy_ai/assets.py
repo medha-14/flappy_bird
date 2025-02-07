128395cdf547f10774a4bb72a076d75a573c0a2d
medha-14 <medha140904@gmail.com>
2025-02-07 10:34:32 +0530

    digits assests

--- flappy_ai/assets.py
+++ flappy_ai/assets.py
@@ -9,9 +9,16 @@ def load_assets():
     background = pygame.image.load("images/background-day.png").convert()
     base_img = pygame.image.load("images/base.png").convert_alpha()
     pipe_img = pygame.image.load("images/pipe-green.png").convert_alpha()
+    
+    # Load digit images for score (assuming you have images named 0.png, 1.png, ..., 9.png)
+    digit_images = {}
+    for i in range(10):
+        digit_images[str(i)] = pygame.image.load(f"images/{i}.png").convert_alpha()
+
     return {
         "bird": bird_images,
         "background": background,
         "base": base_img,
-        "pipe": pipe_img
+        "pipe": pipe_img,
+        "digits": digit_images  # Adding digit images to the dictionary
     }
